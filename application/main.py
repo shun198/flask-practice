8f948a135ec94632aa35d03c5aeb60d5d38dce02
shun198 <shunhiroseluvmri@gmail.com>
2025-01-02 17:30:59 +0900

    👍 add swagger config

--- application/main.py
+++ application/main.py
@@ -3,14 +3,30 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy()
 db.init_app(app)
+# https://flask-migrate.readthedocs.io/en/latest/#api-reference
 migrate = Migrate(app, db)
 migrate.init_app(app, db)
 
+# https://github.com/sveint/flask-swagger-ui
+SWAGGER_URL = "/api/docs"
+API_URL="/static/swagger.yml"
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 class User(db.Model):
     __tablename__ = "users"
